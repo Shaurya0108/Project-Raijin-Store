7092adbd70816a60fb483ae12921b46698aeda0b
Shaurya0108 <shauryadwivedi0108@gmail.com>
2023-09-20 00:06:09 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+export default function App() {
+  return (
+    <html data-theme="black">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <h1 className="text-4xl font-bold tracking-tight text-orange-700 sm:text-8xl">
+          Project Raijin
+        </h1>
+        <div className="relative isolate pt-8">
+          <h4 className="text-4xl font-bold tracking-tight text-white sm:text-3xl">
+            A Rust based game engine.
+          </h4>
+        </div>
+        <div
+          className="absolute top-14 right-14 transform -rotate-90 text-white text-4xl tracking-tight"
+          style={{ transformOrigin: "top right" }}
+        >
+          Store
+        </div>
+      </div>
+    </html>
+  )
+}
---
+++ client/src/pages/landing-page.jsx
@@ -0,0 +1,9 @@
+function Landing() {
+    return (
+      <h1 className="text-3xl font-bold">
+        R-Engine Download
+      </h1>
+    )
+  }
+  
+  export default Landing
